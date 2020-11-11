13717d4e44acc67e59688ac3b635e8d409a65160
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2020-11-11 21:19:41 +0900

    mix format

--- lib/mix/tasks/db_all_delete.ex
+++ lib/mix/tasks/db_all_delete.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.DbAllDelete do
   use Mix.Task
 
   def run(_) do
-    Mix.Task.run "app.start", []
+    Mix.Task.run("app.start", [])
 
     NervesJpChart.Repo.delete_all(NervesJpChart.Measurements.Value)
     NervesJpChart.Repo.delete_all(NervesJpChart.Accounts.User)
